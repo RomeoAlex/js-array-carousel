d0073150ffdd39d15e384fa7ce79fba203d85c05
RomeoAlex <romeo.alessandro17@gmail.com>
2021-11-18 17:31:11 +0100

    milestone 1 e cartelle delle future milestone

---
+++ Milestone 3/js/script.js
@@ -0,0 +1,34 @@
+
+//______________________________MILESTONE 3
+// const flexContainer = document.querySelector('.d-flex');
+// // 9 - creo il ciclo for per creare 100 box con i vari IF per creare box differenti
+
+// for( i = 1; i<= 100; i++){
+//     myValue = i;
+//     const NewBox = document.createElement('div');
+//     NewBox.classList.add('box');
+//     NewBox.classList.add('normal');
+//     const textInside = document.createElement('span');
+//     textInside.append(myValue);
+//     NewBox.append(textInside);
+//     // aggiungo correzione del professore con and
+//     // if( i % 15 === 0 ){
+//     if (( i % 3 === 0  ) && ( i % 5 === 0 )) {   
+//         NewBox.classList.remove('normal');
+//         NewBox.classList.add('fizzbuzz');
+//         textInside.innerHTML = 'fizzbuzz';
+ 
+//     }else if( i % 5 === 0 ) {
+//         NewBox.classList.remove('normal')
+//         NewBox.classList.add('buzz');
+//         textInside.innerHTML = 'buzz';
+//     } else if( i % 3 === 0 ){
+//         NewBox.classList.remove('normal');
+//         NewBox.classList.add('fizz');
+//         textInside.innerHTML = 'fizz';
+//     }
+    
+    
+//     flexContainer.append(NewBox);
+// }
+
