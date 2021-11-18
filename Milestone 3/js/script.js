6e65062ec45427502729430aff2be91022e969b1
RomeoAlex <romeo.alessandro17@gmail.com>
2021-11-19 00:36:31 +0100

    copiato milestone 2 su 3

--- Milestone 3/js/script.js
+++ Milestone 3/js/script.js
@@ -1,19 +1,79 @@
+// ______________________________MILESTONE 2
+const items = [
+    'img/01.jpg',
+    'img/02.jpg',
+    'img/03.jpg',
+    'img/04.jpg',
+    'img/05.jpg'
+];
 
-//______________________________MILESTONE 3
-// const flexContainer = document.querySelector('.d-flex');
-// // 9 - creo il ciclo for per creare 100 box con i vari IF per creare box differenti
+const title = [
+    'Svezia',
+    'Svizzera',
+    'Gran Bretagna',
+    'Germania',
+    'Paradise'
+];
+
+const text = [
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+    'Lorem ipsum',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+    'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+];
+
+// popolo il primo contenitore
+const imgContainer = document.querySelector('.big-image');
+const imgSide = document.querySelector('.side-img');   
+
+for(let i = 0; i < items.length; i++){
+    
+    const thisPath = items[i];
+    const thisTitle = title[i];
+    const thisDescription = text[i];
+    const boxImg = document.createElement('div');
+    boxImg.classList.add('single-img','disappear');
+    boxImg.innerHTML = `<img src="${items[i]}" alt="">` ;
+    const boxText = document.createElement('div');
+    boxText.classList.add('single-title');
+    const boxText1 = document.createElement('h2');
+    boxText1.innerHTML = title[i] ;
+    const boxText2 = document.createElement('p');
+    boxText2.innerHTML = text[i] ;
+    boxText.append(boxText1,boxText2);
+    boxImg.appendChild(boxText);
+    console.log(boxImg);
+    
+    const boxSide = document.createElement('div');
+    boxSide.classList.add('single-side-img','grey-yes');
+    boxSide.innerHTML = `<img src="${items[i]}" alt="">` ;
+    console.log(boxSide);
+    imgContainer.append(boxImg);
+    imgSide.append(boxSide);  
+}
+const boxImg1 = document.querySelector('.single-img')
+    boxImg1.classList.remove('disappear');
+    boxImg1.classList.add('active');
+const boxSide1 = document.querySelector('.single-side-img')
+boxSide1.classList.remove('grey-yes');
+boxSide1.classList.add('grey-no');
+// const arrowUp  = document.querySelector('.top');
+// arrowUp.addEventListener('click' , function(){
+
+// }
+// let activeSelector = document.querySelector('.big-image').firstChild.innerHTML;
+// console.log(activeSelector);
+// activeSelector.remove('disappear');
+// activeSelector.add('active');
+
 
 // for( i = 1; i<= 100; i++){
 //     myValue = i;
 //     const NewBox = document.createElement('div');
-//     NewBox.classList.add('box');
-//     NewBox.classList.add('normal');
-//     const textInside = document.createElement('span');
-//     textInside.append(myValue);
-//     NewBox.append(textInside);
-//     // aggiungo correzione del professore con and
-//     // if( i % 15 === 0 ){
-//     if (( i % 3 === 0  ) && ( i % 5 === 0 )) {   
+//     NewBox.classList.add('single-img');
+    
+//     if () {   
 //         NewBox.classList.remove('normal');
 //         NewBox.classList.add('fizzbuzz');
 //         textInside.innerHTML = 'fizzbuzz';
@@ -27,8 +87,6 @@
 //         NewBox.classList.add('fizz');
 //         textInside.innerHTML = 'fizz';
 //     }
-    
-    
 //     flexContainer.append(NewBox);
 // }
 
